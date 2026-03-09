03f6f6e4d2bd645eb5f46ebf826f464992c3f55c
Dopa <guengant.noe@gmail.com>
2026-03-09 10:54:25 +0100

    feat: cropped image + CLAHE

--- webcam.py
+++ webcam.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+CLAHE = False
 # Smoothing factor: 0 = no update, 1 = no smoothing
 SMOOTHING = 0.3
 prev_faces = []
@@ -33,10 +34,10 @@ def smooth_faces(new_faces: list, prev: list, alpha: float) -> list:
     return smoothed
 
 
-def adjusted_face_detect(frame: np.ndarray) -> np.ndarray:
-    """Return the square drawn image located on the face of the frame."""
+def adjusted_face_detect(im_frame: np.ndarray) -> np.ndarray:
+    """Return the square drawn image located on the face of the provided frame."""
     global prev_faces
-    face_image = frame.copy()
+    face_image = im_frame.copy()
     gray = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
     face_rect = face_cascade.detectMultiScale(
         gray, scaleFactor=1.1, minNeighbors=7, minSize=(80, 80)
@@ -49,6 +50,7 @@ def adjusted_face_detect(frame: np.ndarray) -> np.ndarray:
 
     for (x, y, w, h) in prev_faces:
         x, y, w, h = int(x), int(y), int(w), int(h)
+        face_crop = im_frame[y:y + h, x:x + w].copy()
         # Get classified text here
         classification = "Negative"
         color = (255, 0, 0)
@@ -75,12 +77,18 @@ else:
     rval = False
     frame = None
 
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+
 while rval:
-    cv2.imshow("preview", adjusted_face_detect(frame))
+    if CLAHE:
+        lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+        lab[:, :, 0] = clahe.apply(lab[:, :, 0])
+        frame = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+    cv2.imshow("Webcam preview", adjusted_face_detect(frame))
     rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27:  # exit on ESC
         break
 
 vc.release()
-cv2.destroyWindow("preview")
+cv2.destroyWindow("Webcam preview")
